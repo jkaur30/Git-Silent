f2bd88533b46d21d3d422245d1a3041cb49fd422
vpranavanshu91 <vpranavanshu91@gmail.com>
2017-03-27 17:00:16 -0400

    My first package

---
+++ R/gscom.R
@@ -0,0 +1,19 @@
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+gscom = function(repo='Intro-to-R',user='vpran',com = 'commit')
+{
+  shell(paste("C: & cd C:/Users/",user,'/Documents/GitHub/',repo," & git.exe pull & git.exe add . & git.exe commit -m ",com," & git.exe push origin master",sep = ''))
+  cat("\014")
+  write("", file=".blank")
+  loadhistory(".blank")
+  unlink(".blank")
+}
+
